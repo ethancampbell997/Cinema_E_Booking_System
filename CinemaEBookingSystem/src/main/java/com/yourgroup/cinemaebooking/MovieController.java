a15b6f80725e6235d0fa465ef8e235f1a8f16b42
cjb27407 <cjb27407@uga.edu>
2024-09-24 00:13:10 -0400

    changes

--- CinemaEBookingSystem/src/main/java/com/yourgroup/cinemaebooking/MovieController.java
+++ CinemaEBookingSystem/src/main/java/com/yourgroup/cinemaebooking/MovieController.java
@@ -7,10 +7,14 @@ import org.springframework.web.bind.annotation.*;
 @CrossOrigin(origins = "http://localhost:3000")
 @RequestMapping("/movies")
 public class MovieController {
-    @Autowired
+    //@Autowired
 
     @GetMapping("/search")
-    public String searchTitle(@RequestBody String title) {
+    public String searchTitle(@RequestParam(value = "title") String title) {
+        if (title.isEmpty()) {
+            return "Title parameter is required.";
+        }
+
         MovieObject m = new MovieObject(title);
         if (m.getTitle().equalsIgnoreCase("ERR")) {
             return "ERR";
