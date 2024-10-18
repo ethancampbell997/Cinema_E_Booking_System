5b294a6ba3b5e3c1c94a9f989317834d9ba8c3cd
azadkaz <kazemiazad1@gmail.com>
2024-10-18 11:47:37 -0400

    Update UserController.java
    Added return profile, change password, and edit profile

--- CinemaEBookingSystem/src/main/java/com/yourgroup/cinemaebooking/controllers/UserController.java
+++ CinemaEBookingSystem/src/main/java/com/yourgroup/cinemaebooking/controllers/UserController.java
@@ -19,4 +19,20 @@ public class UserController {
     UserAccess.saveUser(user);
   } // createUser
 
+@PostMapping("/profile")
+  public String returnProfile(@RequestBody LoggedInUser user) {
+        return user.toStringEdit();
+    } // return profile
+
+@PostMapping("/edit")
+  public int editUser(@RequestBody LoggedInUser user, @RequestBody String name, @RequestBody String phone,
+                        @RequestBody String street, @RequestBody String city, @RequestBody String state, @RequestBody String zip) {
+        return UserAccess.updateProfile(user.getUser_id(), name, phone, street, city, state, zip);
+    } // edit user
+
+@PostMapping("/changePassword")
+  public int changePassword(@RequestBody LoggedInUser user, @RequestBody String newPassword) {
+        return UserAccess.updatePassword(user.getUser_id(), newPassword);
+    } // change password
+
 } // UserController
